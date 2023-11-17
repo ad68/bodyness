a78275a321e913a9eceeae40de4214f3b8192cd4
ad68 <47919905+ad68@users.noreply.github.com>
2023-11-17 23:37:28 +0330

    .

---
+++ src/pages/BasicInfo/components/HeightPicker/index.jsx
@@ -0,0 +1,114 @@
+import React, { useState, useContext, useEffect } from "react";
+import "./style.css";
+import Plus from '../../../../assets/images/basicInfo/plus.png'
+import Minus from '../../../../assets/images/basicInfo/minus.png'
+//
+// ────────────────────────────────────────────────────────── I ──────────
+//   :::::: C O M P O N E N T : :  :   :    :     :        :          :
+// ────────────────────────────────────────────────────────────────────
+//
+
+export default function Index({ value }) {
+  // ─── Global Variable ────────────────────────────────────────────────────────────
+  const min = 150;
+  const max = 230;
+  // ─── States ─────────────────────────────────────────────────────────────────────
+  const [height, setHeight] = useState(150); /* cm */
+  const [ft, setFt] = useState();
+  const [unit, setUnit] = useState("cm");
+  // ─── Life Cycle ─────────────────────────────────────────────────────────────────
+  useEffect(() => {
+    let num = String(height * 0.0328084);
+    setFt(num.slice(0, 4));
+    
+  }, [height]);
+  // ─── Functions ──────────────────────────────────────────────────────────────────
+  const increaseHeight = () => {
+    if (height < max) {
+      setHeight(height + 1);
+    }
+  };
+  const decreaseHeight = () => {
+    if (height > min) {
+      setHeight(height - 1);
+    }
+  };
+  //
+  // ──────────────────────────────────────────────────── I ──────────
+  //   :::::: R E N D E R : :  :   :    :     :        :          :
+  // ──────────────────────────────────────────────────────────────
+  //
+  return (
+    <>
+      <section className="unitBox">
+        <button
+          className={`item ${unit === "cm" ? "active" : ""}`}
+          onClick={() => setUnit("cm")}
+        >
+          cm
+        </button>
+        <button
+          className={`item ${unit === "ft" ? "active" : ""}`}
+          onClick={() => setUnit("ft")}
+        >
+          ft
+        </button>
+      </section>
+      <section className="valueChangerBox">
+        <button className="operator" onClick={() => decreaseHeight()}>
+          <img src={Minus} alt="" />
+        </button>
+
+        <section className="text">
+          <span className="value">{unit ==="cm" ? height : ft}</span>
+          <span className="unit">{unit}</span>
+        </section>
+        <button className="operator" onClick={() => increaseHeight()}>
+        <img src={Plus} alt="" />
+        </button>
+      </section>
+      <section className="rangeWrapper">
+        <section className="ruler">
+          <span className="item" style={{ marginTop: 0 }}></span>
+          <span className="item"></span>
+          <span className="item"></span>
+          <span className="item"></span>
+          <span className="item-big"></span>
+          <span className="item"></span>
+          <span className="item"></span>
+          <span className="item"></span>
+          <span className="item-big"></span>
+          <span className="item"></span>
+          <span className="item"></span>
+          <span className="item"></span>
+          <span className="item-big"></span>
+          <span className="item"></span>
+          <span className="item"></span>
+          <span className="item"></span>
+          <span className="item-big"></span>
+          <span className="item"></span>
+          <span className="item"></span>
+          <span className="item"></span>
+          <span className="item-big"></span>
+          <span className="item"></span>
+          <span className="item"></span>
+          <span className="item"></span>
+          <span className="item-big"></span>
+          <span className="item"></span>
+          <span className="item"></span>
+          <span className="item"></span>
+          <span className="item-big"></span>
+          <span className="item"></span>
+          <span className="item"></span>
+        </section>
+        <input
+          type="range"
+          min={String(min)}
+          max={String(max)}
+          value={height}
+          onChange={(e) => setHeight(parseInt(e.target.value))}
+        />
+      </section>
+    </>
+  );
+}
--- src/pages/BasicInfo/steps/Step3.jsx
+++ src/pages/BasicInfo/steps/Step3.jsx
@@ -1,21 +1,23 @@
-import React, { useState, useContext, useEffect } from 'react'
+import React, { useState, useContext, useEffect } from "react";
 import Step from "../../../assets/images/basicInfo/step1L.png";
 import Progress from "../components/Progress";
-import {useTranslation} from 'react-i18next'
+import HeightPicker from "../components/HeightPicker"
+import { useTranslation } from "react-i18next";
+
 //
 // ────────────────────────────────────────────────────────── I ──────────
 //   :::::: C O M P O N E N T : :  :   :    :     :        :          :
 // ────────────────────────────────────────────────────────────────────
 //
 
-export default function Index({setStep,activeStep}) {
+export default function Index({ setStep, activeStep }) {
   // ─── Global Variable ────────────────────────────────────────────────────────────
 
-const {t} = useTranslation();
+  const { t } = useTranslation();
   // ─── States ─────────────────────────────────────────────────────────────────────
- 
+  
   // ─── Life Cycle ─────────────────────────────────────────────────────────────────
- 
+
   // ─── Functions ──────────────────────────────────────────────────────────────────
 
   //
@@ -24,12 +26,15 @@ const {t} = useTranslation();
   // ──────────────────────────────────────────────────────────────
   //
   return (
-     <>
-          <Progress stepPic={Step} activeStep={activeStep} setStep={setStep}/>
+    <>
+      <Progress stepPic={Step} activeStep={activeStep} setStep={setStep} />
       <span className="block text-center title mt-7">
         {t("Basic.WhatsHeight")}
       </span>
-      <button className="letStartBtn" onClick={()=>setStep(4)}>{t("Basic.Next")}</button>
-     </>
-  )
+      <HeightPicker value={100}/>
+      <button className="letStartBtn" onClick={() => setStep(4)}>
+        {t("Basic.Next")}
+      </button>
+    </>
+  );
 }
