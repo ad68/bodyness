2096daef523cd3e81eddf2139e13190d0bf23bb6
unknown <alidargahi1368@gmail.com>
2023-12-18 02:14:57 +0330

    .

--- src/pages/RoadMap/Section1/index.jsx
+++ src/pages/RoadMap/Section1/index.jsx
@@ -32,7 +32,7 @@ export default function Index() {
   // ──────────────────────────────────────────────────────────────
   //
   return (
-    <section className='section1'>
+    <section className="section1">
       <section className="header">
         <span className="levelNumber">1</span>
         <span className="text mr-10">
@@ -46,27 +46,87 @@ export default function Index() {
         className="flex justify-center items-end relative roadBox m-auto mt-36"
         style={{ direction: 'ltr' }}
       >
-        
+        <header>
+          <span className='title'>عنوان</span>
+          <p>
+          لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد 
+          </p>
+        </header>
         <section className="vec1">
           <img src={vector1} alt="" />
-          <img src={fat} alt="" className='fat' />
-          <img src={target} alt="" className='icon' />
+          <img src={fat} alt="" className="fat" />
+          <img src={target} alt="" className="icon" />
+          <section className="infoBox">
+            <section className="title">
+              <span className="number">1</span>
+              <span className="text">عنوان</span>
+              <p>
+                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
+                استفاده از طراحان گرافیک است، ه
+              
+              </p>
+            </section>
+          </section>
         </section>
         <section className="vec2">
           <img src={vector2} alt="" />
-          <img src={calory} alt="" className='icon' />
+          <img src={calory} alt="" className="icon" />
+          <section className="infoBox">
+            <section className="title">
+              <span className="number">2</span>
+              <span className="text">عنوان</span>
+              <p>
+                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
+                استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله
+              
+              </p>
+            </section>
+          </section>
         </section>
         <section className="vec3">
           <img src={vector3} alt="" />
-          <img src={plate} alt="" className='icon' />
+          <img src={plate} alt="" className="icon" />
+          <section className="infoBox">
+            <section className="title">
+              <span className="number">3</span>
+              <span className="text">عنوان</span>
+              <p>
+                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
+                استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله
+              
+              </p>
+            </section>
+          </section>
         </section>
         <section className="vec4">
           <img src={vector4} alt="" />
-          <img src={damble} alt="" className='icon' />
+          <img src={damble} alt="" className="icon" />
+          <section className="infoBox">
+            <section className="title">
+              <span className="number">4</span>
+              <span className="text">عنوان</span>
+              <p>
+                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
+                استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله
+              
+              </p>
+            </section>
+          </section>
         </section>
         <section className="vec5">
           <img src={vector5} alt="" />
-          <img src={muscle} alt="" className='muscle' />
+          <img src={muscle} alt="" className="muscle" />
+          <section className="infoBox">
+            <section className="title">
+              <span className="number">5</span>
+              <span className="text">عنوان</span>
+              <p>
+                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، 
+                
+              
+              </p>
+            </section>
+          </section>
         </section>
       </section>
     </section>
